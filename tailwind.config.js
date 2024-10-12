636d682563aef5c2f30457928d9b618e105ca814
Miguel de Jesús López López <winalllpz@gmail.com>
2024-10-11 23:19:27 -0600

    Integración con vue,  typescript y tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.vue",
+        "./resources/**/*.js",
+        "./resources/**/*.ts",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+
+export default defineConfig({
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            'vue': 'vue/dist/vue.esm-bundler.js', // Alias para habilitar runtime + template compilation
+            '@': path.resolve(__dirname, './resources/js'),
+        },
+    },
+    server: {
+        host: '0.0.0.0', // Escucha en todas las interfaces de red
+        port: 5173,       // Puerto de Vite
+        strictPort: true, // Asegura que use este puerto
+        hmr: {
+            host: 'localhost', // Usa localhost para la recarga en caliente
+        },
+    },
+});
