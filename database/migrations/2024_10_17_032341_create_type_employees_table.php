95584def1f780f7375536b02dad5252994ba321d
Miguel de Jesús López López <winalllpz@gmail.com>
2024-10-16 22:05:04 -0600

    Migración type_employees y employees

---
+++ database/migrations/2024_10_17_032341_create_type_employees_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('type_employees', function (Blueprint $table) {
+            $table->id();
+            $table->integer('type')->nullable(false);
+            $table->integer('status')->nullable(false)->default(1);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('type_employees');
+    }
+};
